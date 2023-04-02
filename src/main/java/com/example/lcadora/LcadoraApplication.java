afdf330b436d3c0b3c215dda442dac89762f5230
otanerifpb <renato.silva.pro@gmail.com>
2023-04-02 13:31:33 -0300

    Create LcadoraApplication.java

---
+++ src/main/java/com/example/lcadora/LcadoraApplication.java
@@ -0,0 +1,13 @@
+package com.example.lcadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcadoraApplication.class, args);
+	}
+
+}
