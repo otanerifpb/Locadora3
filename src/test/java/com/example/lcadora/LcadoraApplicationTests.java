8418ee25aa03473de9223aff00e84c1c9bef53df
otanerifpb <renato.silva.pro@gmail.com>
2023-04-02 13:34:56 -0300

    Create LcadoraApplicationTests.java

---
+++ src/test/java/com/example/lcadora/LcadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lcadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
